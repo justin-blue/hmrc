62e29590be7bf071d4be502b90bdf4b5037f277a
justin-blue <jagodzo@gmail.com>
2018-04-16 11:01:48 +0100

    added checkout with offer

--- app/src/main/java/reallyhandyapps/com/hmrc/Shop.java
+++ app/src/main/java/reallyhandyapps/com/hmrc/Shop.java
@@ -11,4 +11,18 @@ public class Shop {
         }
         return cost;
     }
+
+    public int checkoutWithOffer(ArrayList<Item> items){
+        int cost = 0;
+        for(Item item:items){
+            cost += item.getPrice();
+            if(item instanceof Apple){
+                if(items.indexOf(item) % 2 == 1){cost -= item.getPrice();}
+            }else{
+                //must be an orange as shop sells only Apples and Oranges
+                if(items.indexOf(item) % 3 == 1){cost -= item.getPrice();}
+            }
+        }
+        return  cost;
+    }
 }
--- app/src/test/java/reallyhandyapps/com/hmrc/ShopUnitTest.java
+++ app/src/test/java/reallyhandyapps/com/hmrc/ShopUnitTest.java
@@ -16,4 +16,59 @@ public class ShopUnitTest {
         items.add(new Orange("Orange",25));
         Assert.assertEquals(85, shop.checkout(items));
     }
+
+    @Test
+    public void testCheckoutWithOfferForSingleApple(){
+        Shop shop = new Shop();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Apple("Apple",60));
+        Assert.assertEquals(60,shop.checkoutWithOffer(items));
+    }
+
+    @Test
+    public void testCheckoutWithOfferForSingleOrange(){
+        Shop shop = new Shop();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Orange("Orange",25));
+        Assert.assertEquals(25,shop.checkoutWithOffer(items));
+    }
+    @Test
+    public void testCheckoutWithOfferForTwoApples(){
+        Shop shop = new Shop();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Apple("Apple",60));
+        items.add(new Apple("Apple",60));
+        Assert.assertEquals(60,shop.checkoutWithOffer(items));
+    }
+
+    @Test
+    public void testCheckoutWithOfferForThreeApples(){
+        Shop shop = new Shop();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Apple("Apple",60));
+        items.add(new Apple("Apple",60));
+        items.add(new Apple("Apple",60));
+        Assert.assertEquals(120,shop.checkoutWithOffer(items));
+    }
+
+    @Test
+    public  void testCheckoutWithOfferForThreeOranges(){
+        Shop shop = new Shop();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Orange("Orange",25));
+        items.add(new Orange("Orange",25));
+        items.add(new Orange("Orange",25));
+        Assert.assertEquals(50,shop.checkoutWithOffer(items));
+    }
+
+    @Test
+    public void testCheckoutWithOfferForFourOranges(){
+        Shop shop = new Shop();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Orange("Orange",25));
+        items.add(new Orange("Orange",25));
+        items.add(new Orange("Orange",25));
+        items.add(new Orange("Orange",25));
+        Assert.assertEquals(75,shop.checkoutWithOffer(items));
+    }
 }
